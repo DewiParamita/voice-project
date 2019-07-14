ed140d72e740efb3d052007a537d2129f53c80d0
DewiParamita <dewiparamita33@gmail.com>
2019-07-14 23:57:22 +0700

    Update Untitled1.py

--- Untitled1.py
+++ Untitled1.py
@@ -4,5 +4,15 @@
 # In[3]:
 
 
-print("tesssss yaaa")
+from __future__ import print_function
+from ipywidgets import interact, interactive, fixed, interact_manual
+import ipywidgets as widgets
+from ipywidgets import FloatSlider
 
+def slow_function(i):
+    print(int(i),list(x for x in range(int(i)) if
+                str(x)==str(x)[::-1] and
+                str(x**2)==str(x**2)[::-1]))
+    return
+
+interact_manual(slow_function,i=FloatSlider(min=1e5, max=1e7, step=1e5));
